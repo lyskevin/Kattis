c8fc148d1372d0f3508b69714ab1ccca8cc5e012
Kevin Lim <lyskevin@gmail.com>
2019-08-03 11:03:46 +0800

    Added the FastReader class to the solution code for Booking a Room

--- Booking a Room/Main.java
+++ Booking a Room/Main.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.StringTokenizer;
 
 /**
  * @author lyskevin
  */
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        FastReader fr = new FastReader();
         boolean[] bookedRooms = new boolean[100];
-        int numberOfRooms = sc.nextInt();
-        int numberOfBookedRooms = sc.nextInt();
+        int numberOfRooms = fr.nextInt();
+        int numberOfBookedRooms = fr.nextInt();
         if (numberOfBookedRooms == numberOfRooms) {
             System.out.println("too late");
         } else {
             for (int i = 0; i < numberOfBookedRooms; i++) {
-                int bookedRoom = sc.nextInt();
+                int bookedRoom = fr.nextInt();
                 bookedRooms[bookedRoom - 1] = true;
             }
             for (int i = 0; i < 100; i++) {
@@ -25,3 +28,65 @@ public class Main {
         }
     }
 }
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastReader 
+{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader() 
+    { 
+        br = new BufferedReader(new
+                InputStreamReader(System.in)); 
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
