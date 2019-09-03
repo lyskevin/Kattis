62a4cb025f6af8180c4a27f5382b858068dfbc4a
Kevin Lim <lyskevin@gmail.com>
2019-09-03 18:01:28 +0800

    Added solution code and README for Sum Kind of Problem

---
+++ Sum Kind of Problem/Main.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int numberOfTestCases = fio.nextInt();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < numberOfTestCases; i++) {
+            int caseNumber = fio.nextInt();
+            int n = fio.nextInt();
+            int sum = (1 + n) * n / 2;
+            int evenSum = sum * 2;
+            int oddSum = evenSum - n;
+            output.append(caseNumber + " " + sum + " " + oddSum + " " + evenSum + "\n");
+        }
+        fio.print(output);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
