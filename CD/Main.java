839e68d00d67f842478bfeaa3abf0746b070308f
Kevin Lim <lyskevin@gmail.com>
2019-09-20 12:02:48 +0800

    Added solution code and README for CD

---
+++ CD/Main.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int m = fio.nextInt();
+        int n = fio.nextInt();
+        while (m > 0 || n > 0) {
+            HashSet<Integer> jackCD = new HashSet<>();
+            for (int i = 0; i < m; i++) {
+                jackCD.add(fio.nextInt());
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (jackCD.contains(fio.nextInt())) {
+                    count++;
+                }
+            }
+            fio.println(count);
+            m = fio.nextInt();
+            n = fio.nextInt();
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
