535c13c9f6385fafe2e4d352780140a95b04414f
Kevin Lim <lyskevin@gmail.com>
2019-09-23 17:52:52 +0800

    Updated solution code for Join Strings

--- Join Strings/Main.java
+++ Join Strings/Main.java
@@ -1,28 +1,29 @@
-import java.io.BufferedReader; 
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
 import java.io.IOException; 
-import java.io.InputStreamReader; 
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.StringTokenizer;
-import java.lang.StringBuilder;
 
 /**
  * @author lyskevin
  */
 public class Main {
     public static void main(String[] args) {
-        FastReader fr = new FastReader();
-        int numberOfStrings = fr.nextInt();
+        FastIO fio = new FastIO();
+        int numberOfStrings = fio.nextInt();
         String[] strings = new String[numberOfStrings];
         MyList[] myLists = new MyList[numberOfStrings];
         for (int i = 0; i < numberOfStrings; i++) {
-            strings[i] = fr.next();
+            strings[i] = fio.next();
             ListNode listNode = new ListNode(i);
             MyList myList = new MyList(listNode, listNode);
             myLists[i] = myList;
         }
         int finalStart = 0;
         for (int j = 0; j < numberOfStrings - 1; j++) {
-            int start = fr.nextInt() - 1;
-            int end = fr.nextInt() - 1;
+            int start = fio.nextInt() - 1;
+            int end = fio.nextInt() - 1;
             MyList list1 = myLists[start];
             MyList list2 = myLists[end];
             list1.tail.next = list2.head;
@@ -33,10 +34,11 @@ public class Main {
         MyList finalList = myLists[finalStart];
         ListNode listNode = finalList.head;
         while (listNode != null) {
-            output.append(strings[listNode.index]);
+            fio.print(strings[listNode.index]);
             listNode = listNode.next;
         }
-        System.out.println(output);
+        fio.println();
+        fio.close();
     }
 }
 
@@ -69,15 +71,16 @@ class ListNode {
  * Fast I/O
  * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
  */
-class FastReader 
+class FastIO extends PrintWriter 
 { 
     BufferedReader br; 
-    StringTokenizer st; 
+    StringTokenizer st;
 
-    public FastReader() 
+    public FastIO() 
     { 
+        super(new BufferedOutputStream(System.out)); 
         br = new BufferedReader(new
-                InputStreamReader(System.in)); 
+                InputStreamReader(System.in));
     } 
 
     String next() 
@@ -125,3 +128,4 @@ class FastReader
         return str; 
     } 
 }
+
