b3779f2b5e05b08ee450dbb57f5b3f1481ec6d0d
Kevin Lim <lyskevin@gmail.com>
2019-09-26 12:11:11 +0800

    Updated import statements in FastIO.java

--- FastIO.java
+++ FastIO.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedOutputStream;
-import java.io.IOException; 
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * Fast I/O
