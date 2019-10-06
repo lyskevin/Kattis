b45047fccb12bff584e472b0344325f3a9947112
Kevin Lim <lyskevin@gmail.com>
2019-10-06 12:30:34 +0800

    Added solution code and README for Volim

---
+++ Volim/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int player = fio.nextInt() - 1;
+        int totalTime = 0;
+        int numberOfQuestions = fio.nextInt();
+        for (int i = 0; i < numberOfQuestions; i++) {
+            int time = fio.nextInt();
+            totalTime += time;
+            String outcome = fio.next();
+            if (totalTime >= 210) {
+                fio.println(player + 1);
+                break;
+            } else if (outcome.equals("T")) {
+                player = (player + 1) % 8;
+            }
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
