87feac7fd80f329245400dcd274bf37d6159bc99
Kevin Lim <lyskevin@gmail.com>
2019-10-22 16:47:13 +0800

    Added solution code and README for Relocation

---
+++ Relocation/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        int q = fio.nextInt();
+        HashMap<Integer, Integer> locations = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            locations.put(i, fio.nextInt());
+        }
+        for (int i = 0; i < q; i++) {
+            int queryType = fio.nextInt();
+            if (queryType == 1) {
+                locations.put(fio.nextInt(), fio.nextInt());
+            } else {
+                fio.println(Math.abs(locations.get(fio.nextInt()) - locations.get(fio.nextInt())));
+            }
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
