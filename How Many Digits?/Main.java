25c05d97e3a86bd7d8c883f9c27627dffad8dd7f
Kevin Lim <lyskevin@gmail.com>
2019-12-13 18:27:23 +0700

    Added solution code and README for How Many Digits?

---
+++ How Many Digits?/Main.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int[] digits = new int[1000001];
+        digits[0] = 1;
+        double currentNumberOfDigits = 1.0;
+        for (int i = 1; i <= 1000000; i++) {
+            currentNumberOfDigits += Math.log10(i);
+            digits[i] = (int) currentNumberOfDigits;
+        }
+        int n = fio.nextInt();
+        while (true) {
+            try {
+                fio.println(digits[n]);
+                n = fio.nextInt();
+            } catch (NullPointerException e) {
+                break;
+            }
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
