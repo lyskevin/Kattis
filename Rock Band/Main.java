1ff8a39d24fa15905105c384d1ddee6e7c674971
Kevin Lim <lyskevin@gmail.com>
2020-01-11 23:51:49 +0800

    Added solution code and README for Rock Band

---
+++ Rock Band/Main.java
@@ -0,0 +1,115 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int m = fio.nextInt();
+        int s = fio.nextInt();
+        int[] songRanks = new int[s + 1];
+        int[] firstList = new int[s];
+        // Store the first input list (in order to get the output in sorted order later on)
+        for (int i = 0; i < s; i++) {
+            int song = fio.nextInt();
+            firstList[i] = song;
+            songRanks[song] = i;
+        }
+        // Store the maximum rank of each song
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < s; j++) {
+                int song = fio.nextInt();
+                songRanks[song] = Math.max(songRanks[song], j);
+            }
+        }
+        int index = 0;
+        int song = firstList[index];
+        // Jump to the song whose maximum rank matches its index in the input list
+        // This works since we store the maximum rank among all input lists
+        // We stop when we reach the first such song since we want the smallest such set of songs
+        while (songRanks[song] != index) {
+            index = songRanks[song];
+            song = firstList[index];
+        }
+        // Sort and print the required set of songs
+        int[] sortedSongs = new int[index + 1];
+        for (int i = 0; i < index + 1; i++) {
+            sortedSongs[i] = firstList[i];
+        }
+        fio.println(index + 1);
+        Arrays.sort(sortedSongs);
+        for (int i = 0; i < index + 1; i++) {
+            if (i > 0) {
+                fio.print(" ");
+            }
+            fio.print(sortedSongs[i]);
+        }
+        fio.println();
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
