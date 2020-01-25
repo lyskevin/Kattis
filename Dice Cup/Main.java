5ba526757f0fe7c5a3da403c4bb7b024de416aab
Kevin Lim <lyskevin@gmail.com>
2020-01-25 19:19:40 +0800

    Added solution code and README for Dice Cup

---
+++ Dice Cup/Main.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        int m = fio.nextInt();
+        int[] frequencies = new int[n + m + 1];
+        int maxFrequency = 0;
+        // Count frequencies of all possible outcomes
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                frequencies[i + j] += 1;
+                maxFrequency = Math.max(maxFrequency, frequencies[i + j]);
+            }
+        }
+        // Print outcome(s) with the highest frequency
+        for (int i = 1; i <= n + m; i++) {
+            if (frequencies[i] == maxFrequency) {
+                fio.println(i);
+            }
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
