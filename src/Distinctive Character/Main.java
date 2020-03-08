b83c8ba27e566127759b7c0985bd367e7f057d87
Kevin Lim <lyskevin@gmail.com>
2020-03-08 15:52:41 +0800

    Added solution code and README for Distinctive Character

---
+++ src/Distinctive Character/Main.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        int k = fio.nextInt();
+        Queue<Integer> bfsQueue = new LinkedList<>();
+        boolean[] visited = new boolean[2000000];
+        for (int i = 0; i < n; i++) {
+            String s = fio.nextLine();
+            int character = Integer.parseInt(s, 2);
+            bfsQueue.add(character);
+            visited[character] = true;
+        }
+        int result = 0;
+        ArrayList<Integer> bitMasks = new ArrayList<>();
+        for (int i = 1, bitMask = 0x1; i <= k; i++) {
+            bitMasks.add(bitMask);
+            bitMask = bitMask << 1;
+        }
+        while (!bfsQueue.isEmpty()) {
+            int character = bfsQueue.poll();
+            result = character;
+            for (int i = 0; i < k; i++) {
+                int newCharacter = character ^ bitMasks.get(i);
+                if (!visited[newCharacter]) {
+                    visited[newCharacter] = true;
+                    bfsQueue.add(newCharacter);
+                }
+            }
+        }
+        String s = Integer.toBinaryString(result);
+        for (int i = 0; i < k - s.length(); i++) {
+            fio.print(0);
+        }
+        fio.println(s);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Distinctive Character/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/distinctivecharacter
+
+## Personal Thoughts
+
+Problem that I attempted while I was a Teaching Assistant (TA) for NUS' CS2040 (Data Structures and Algorithms) course in Semester 2 of AY19/20. Can be solved by running multi-source BFS and choosing the bitmask which is at the end of the BFS queue, i.e. the one furthest away from all of the given inputs. To find each neighbour of the bitmask being polled from the queue, we iteratively XOR it with a bitmask containing 1 at a single position, and 0 elsewhere.
+
