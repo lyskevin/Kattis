058c43ce81e288e59434e48e4cd138057f407420
Kevin Lim <lyskevin@gmail.com>
2020-03-24 23:26:58 +0800

    Added solution code for Reduced ID Numbers

---
+++ src/Reduced ID Numbers/Main.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int g = fio.nextInt();
+        int[] numbers = new int[g];
+        for (int i = 0; i < g; i++) {
+            numbers[i] = fio.nextInt();
+        }
+        HashSet<Integer> remainders = new HashSet<>();
+        int m = g - 1;
+        // Generate remainders until g unique remainders are found for some m
+        while (remainders.size() < g) {
+            m++;
+            remainders = new HashSet<>();
+            for (int i : numbers) {
+                int remainder = i % m;
+                if (remainders.contains(remainder)) {
+                    break;
+                }
+                remainders.add(remainder);
+            }
+        }
+        fio.println(m);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
