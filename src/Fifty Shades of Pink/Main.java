529f91b8263e781ff3b934f3b21fd8202e52176d
Kevin Lim <lyskevin@gmail.com>
2020-04-21 23:09:42 +0800

    Added solution code and README for Fifty Shades of Pink

---
+++ src/Fifty Shades of Pink/Main.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            String lowerCaseInput = fio.nextLine().toLowerCase();
+            if (lowerCaseInput.contains("pink") || lowerCaseInput.contains("rose")) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            fio.println(count);
+        } else {
+            fio.println("I must watch Star Wars with my daughter");
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
