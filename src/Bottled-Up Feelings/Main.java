2e53e349a20abf4872d3b71d1df893f0cbe84e25
Kevin Lim <lyskevin@gmail.com>
2020-04-26 13:21:21 +0800

    Added solution code and README for Bottled-Up Feelings

---
+++ src/Bottled-Up Feelings/Main.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int s = fio.nextInt();
+        int v1 = fio.nextInt();
+        int v2 = fio.nextInt();
+        boolean hasSolution = false;
+        // Start from largest possible number of bottles with volume v1
+        // Stop after finding the first valid assignment (guaranteed to use the least number of bottles)
+        for (int i = s / v1; i >= 0 && !hasSolution; i--) {
+            int remainingVolume = s - i * v1;
+            if (remainingVolume % v2 == 0) {
+                fio.println(i + " " + (remainingVolume / v2));
+                hasSolution = true;
+            }
+        }
+        if (!hasSolution) {
+            fio.println("Impossible");
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Bottled-Up Feelings/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/bottledup
+
+## Personal Thoughts
+
+Problem that I attempted while I was a Teaching Assistant (TA) for NUS' CS2040 (Data Structures and Algorithms) course in Semester 2 of AY19/20. Can be solved by using iteration to find a pair of integers that satisfy the condition. Start from the largest integer that can be multiplied by v1 so that the first valid assignment uses the least number of bottles (i.e. is the desired solution).
+
