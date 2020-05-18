a14ae9f9ceef2362975ec98e3948ff5f5264de3e
Kevin Lim <lyskevin@gmail.com>
2020-05-18 13:32:31 +0800

    Added solution code and README for The Owl and the Fox

---
+++ src/The Owl and the Fox/Main.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int t = fio.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = fio.nextInt();
+            int digitSum = getDigitSum(n);
+            for (int j = n - 1; j >= 0; j--) {
+                // Search for highest number < n which has a digit sum which is 1 less than that of n
+                if (getDigitSum(j) == digitSum - 1) {
+                    fio.println(j);
+                    break;
+                }
+            }
+        }
+        fio.close();
+    }
+
+    private static int getDigitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/The Owl and the Fox/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/owlandfox
+
+## Personal Thoughts
+
+Problem that I attempted during the summer holidays of 2020. Can be solved by using complete search to try numbers until one satisfies the given conditions.
+
