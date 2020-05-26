d410718b716de28f281e4cb8a5cde43a8251115c
Kevin Lim <lyskevin@gmail.com>
2020-05-27 00:30:30 +0800

    Add solution code and README for Suspension Bridges

---
+++ src/Suspension Bridges/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int d = fio.nextInt();
+        int s = fio.nextInt();
+        double low = -1000000000000000.0;
+        double high = 1000000000000000.0;
+        double a = 0.0;
+        for (int i = 0; i < 100; i++) { // Should converge within 100 iterations
+            a = (low + high) / 2;
+            double test = a + s - a * Math.cosh(d / (2 * a));
+            if (test >= 0.0) {
+                high = a;
+            } else {
+                low = a;
+            }
+        }
+        fio.println(2 * a * Math.sinh(d / (2 * a)));
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Suspension Bridges/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/suspensionbridges
+
+## Personal Thoughts
+
+Problem that I attempted during the summer holidays of 2020. Can be solved by binary searching for the appropriate value of a (and thus l).
+
