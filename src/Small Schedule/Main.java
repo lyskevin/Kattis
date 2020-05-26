6970a9201a5eaaed62ed5608ae675186b7628452
Kevin Lim <lyskevin@gmail.com>
2020-05-27 00:58:25 +0800

    Add solution code and README for Small Schedule

---
+++ src/Small Schedule/Main.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int q = fio.nextInt();
+        int m = fio.nextInt();
+        int s = fio.nextInt();
+        int l = fio.nextInt();
+        
+        // Greedily allocate q-second time slots
+        int result = (l / m) * q;
+        result = l % m > 0 ? result + q : result;
+
+        // Use one-second time slots to fill up spaces left by q-second time slots
+        s -= ((m - (l % m)) % m) * q;
+
+        // Use up remaining one-second time slots
+        if (s > 0) {
+            result += s / m;
+            result = s % m > 0 ? result + 1 : result;
+        }
+
+        fio.println(result);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
