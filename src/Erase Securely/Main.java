a436b5fcf22065d3800bd7c75fd5a36edb0ee24a
Kevin Lim <lyskevin@gmail.com>
2020-05-31 16:10:36 +0800

    Add solution code and README for Erase Securely

---
+++ src/Erase Securely/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        String s = fio.nextLine();
+        String e = fio.nextLine();
+        boolean isSuccessful = true;
+        for (int i = 0; i < s.length() && isSuccessful; i++) {
+            if ((n % 2 == 0 && s.charAt(i) != e.charAt(i))
+                    || (n % 2 != 0 && s.charAt(i) == e.charAt(i))) {
+                isSuccessful = false;
+            }
+        }
+        if (isSuccessful) {
+            fio.println("Deletion succeeded");
+        } else {
+            fio.println("Deletion failed");
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Erase Securely/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/erase
+
+## Personal Thoughts
+
+Problem that I attempted during the summer holidays of 2020. Can be solved by taking the inverse of the input string if n is even, and the input string itself otherwise.
+
