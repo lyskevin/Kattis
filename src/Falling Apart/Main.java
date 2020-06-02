aa89cc7774585e8f61e63301f8f942b2f4017bec
Kevin Lim <lyskevin@gmail.com>
2020-06-02 16:08:14 +0800

    Add solution code and README for Falling Apart

---
+++ src/Falling Apart/Main.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        int[] pieces = new int[n];
+        for (int i = 0; i < n; i++) {
+            pieces[i] = fio.nextInt();
+        }
+        Arrays.sort(pieces);
+        int a = 0, b = 0;
+        boolean isFirst = true;
+        // Alice and Bob choose greedily
+        for (int i = n - 1; i >= 0; i--) {
+            if (isFirst) {
+                a += pieces[i];
+            } else {
+                b += pieces[i];
+            }
+            isFirst = !isFirst;
+        }
+        fio.println(a + " " + b);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Falling Apart/README.md
@@ -0,0 +1,7 @@
+# Link
+
+https://open.kattis.com/problems/fallingapart
+
+## Personal Thoughts
+Problem that I attempted during the summer holidays of 2020. Can be solved by sorting the n values and then letting Alice and Bob choose greedily.
+
