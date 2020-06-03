36298b555317a16022bb26facaf406b4e350eac9
Kevin Lim <lyskevin@gmail.com>
2020-06-03 20:54:57 +0800

    Add solution code and README for ICPC Team Selection

---
+++ src/ICPC Team Selection/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int t = fio.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = fio.nextInt();
+            int[] scores = new int[3 * n];
+            for (int j = 0; j < 3 * n; j++) {
+                scores[j] = fio.nextInt();
+            }
+            Arrays.sort(scores);
+            int s = 0;
+            for (int j = n; j < 3 * n; j += 2) { // Pick every second score from the back
+                s += scores[j];
+            }
+            fio.println(s);
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/ICPC Team Selection/README.md
@@ -0,0 +1,7 @@
+# Link
+
+https://open.kattis.com/problems/icpcteamselection
+
+## Personal Thoughts
+Problem that I attempted during the summer holidays of 2020. Can be solved by greedily picking every second score from the back to find the highest possible sum (i.e. every team comprises of the lowest remaining score and the two highest remaining scores).
+
