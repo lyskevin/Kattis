dc0ce37e3336a043cfc93b507751a69c8ba68391
Kevin Lim <lyskevin@gmail.com>
2020-06-04 10:13:43 +0800

    Add solution code and README for Text Messaging Outrage

---
+++ src/Text Messaging Outrage/Main.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int n = fio.nextInt();
+        for (int i = 0; i < n; i++) {
+            long p = fio.nextLong();
+            long k = fio.nextLong();
+            int l = fio.nextInt();
+            long[] frequencies = new long[l];
+            for (int j = 0; j < l; j++) {
+                frequencies[j] = fio.nextLong();
+            }
+            Arrays.sort(frequencies);
+            long minimum = 0l;
+            // Greedily pick keys to place letters on (highest frequency first)
+            for (int j = 0; j < l; j++) {
+                minimum += (frequencies[l - j - 1]) * ((j / k) + 1);
+            }
+            fio.println("Case #" + (i + 1) + ": " + minimum);
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Text Messaging Outrage/README.md
@@ -0,0 +1,7 @@
+# Link
+
+https://open.kattis.com/problems/textmessaging
+
+## Personal Thoughts
+Problem that I attempted during the summer holidays of 2020. Can be solved by greedily picking keys to place letters on (by decreasing frequency).
+
