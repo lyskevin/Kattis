a561381ecc8ec77919bb7c85fa0c266aae66974d
Kevin Lim <lyskevin@gmail.com>
2020-06-04 10:26:37 +0800

    Add solution code and README for Wood Cutting

---
+++ src/Wood Cutting/Main.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int t = fio.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = fio.nextInt();
+            long[] times = new long[n];
+            for (int j = 0; j < n; j++) {
+                int w = fio.nextInt();
+                for (int k = 0; k < w; k++) {
+                    times[j] += fio.nextLong();
+                }
+            }
+            Arrays.sort(times);
+            long totalTime = 0l;
+            long currentTime = 0l;
+            // Pick customers greedily (shortest time first)
+            for (int j = 0; j < n; j++) {
+                currentTime += times[j];
+                totalTime += currentTime;
+            }
+            fio.println(1.0 * totalTime / n);
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
