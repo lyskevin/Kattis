98f0b45ecd0b1eced5d6af0ef47525f0911cfe05
Kevin Lim <lyskevin@gmail.com>
2020-06-10 11:25:48 +0800

    Add solution code and README for Ants

---
+++ src/Ants/Main.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int t = fio.nextInt();
+        for (int i = 0; i < t; i++) {
+            int l = fio.nextInt();
+            int n = fio.nextInt();
+            int min = 0, max = 0;
+            for (int j = 0; j < n; j++) {
+                int p = fio.nextInt();
+                int d1 = p - 0;
+                int d2 = l - p;
+                // Shortest distance from ant closest to the center
+                min = Math.max(min, Math.min(d1, d2));
+                // Distance from ant furthest from either end
+                max = Math.max(max, Math.max(d1, d2));
+            }
+            fio.println(min + " " + max);
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Ants/README.md
@@ -0,0 +1,9 @@
+# Link
+
+https://open.kattis.com/problems/ants
+
+## Personal Thoughts
+Problem that I attempted during the summer holidays of 2020. Can be solved by greedily choosing directions.
+Minimum time: Shortest distance from end of the pole by ant closest to the center of the pole.
+Maximum time: Longest distance from end of the pole by ant furthest away from said end.
+
