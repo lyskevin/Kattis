dba683e03d5bf8e92c26a47090517e075bcfdfef
Kevin Lim <lyskevin@gmail.com>
2020-07-29 13:05:43 +0800

    Add solution code and README for Triple Texting

---
+++ src/Triple Texting/Main.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        String s = fio.nextLine();
+        int length = s.length() / 3;
+        for (int i = 0; i < length; i++) {
+            if (s.charAt(i) == s.charAt(i + length) || s.charAt(i) == s.charAt(i + (length << 1))) {
+                fio.print(s.charAt(i));
+            } else {
+                fio.print(s.charAt(i + length));
+            }
+        }
+        fio.println();
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Triple Texting/README.md
@@ -0,0 +1,7 @@
+# Link
+
+https://open.kattis.com/problems/tripletexting
+
+## Personal Thoughts
+Problem that I attempted during the summer holidays of 2020. Can be solved by printing any character that appears at least twice at a given index of the three strings.
+
