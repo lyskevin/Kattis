1f6d68a07f441e6e82add4ead782823c7a1b30ac
Kevin Lim <lyskevin@gmail.com>
2020-08-08 02:48:16 +0800

    Add solution code and README for Pripreme

---
+++ src/Pripreme/Main.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int N = fio.nextInt();
+
+        long total = 0l;
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            int time = fio.nextInt();
+            total += time;
+            max = Math.max(max, time);
+        }
+       
+        // Answer is the greater of the total time and twice the maximum individual time
+        fio.println(Math.max(total, max << 1));
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Pripreme/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/pripreme
+
+## Personal Thoughts
+
+Problem that I attempted during the summer holidays of 2020. It can be proven that the answer is the greater of the sum of all times and twice the maximum individual time.
+
