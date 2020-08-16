de806f8d5ddd8b2f55751799b092c78f59a0b181
Kevin Lim <lyskevin@gmail.com>
2020-08-16 23:51:49 +0800

    Add solution code and README for Broken Swords

---
+++ src/Broken Swords/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int N = fio.nextInt();
+        int[] count = new int[2];
+
+        for (int i = 0; i < N; i++) {
+            String s = fio.nextLine();
+            for (int j = 0; j < 4; j++) {
+                count[j >> 1] += s.charAt(j) == '0' ? 1 : 0;
+            }
+        }
+
+        int swords = Math.min(count[0], count[1]) >> 1;
+        fio.println(swords + " " + (count[0] - (swords << 1)) + " " + (count[1] - (swords << 1)));
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
