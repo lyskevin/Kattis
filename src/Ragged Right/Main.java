52b94ed479b6bd3a078b2c782e6feaa0d5efb242
Kevin Lim <lyskevin@gmail.com>
2020-08-24 23:50:30 +0800

    Add solution code and README for Ragged Right

---
+++ src/Ragged Right/Main.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        ArrayList<String> paragraph = new ArrayList<>();
+
+        int max = 0;
+        while (true) {
+            try {
+                String s = fio.nextLine();
+                max = Math.max(max, s.length());
+                paragraph.add(s);
+            } catch (NullPointerException e) {
+                break;
+            }
+        }
+
+        long total = 0l;
+        for (int i = 0; i < paragraph.size() - 1; i++) {
+            String sentence = paragraph.get(i);
+            total += (max - sentence.length()) * (max - sentence.length());
+        }
+
+        fio.println(total);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Ragged Right/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/raggedright
+
+## Personal Thoughts
+
+Problem that I attempted during the summer holidays of 2020. Self-explanatory problem.
+
