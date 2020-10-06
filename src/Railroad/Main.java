101a89e7b4cb3c08e801160c9a4c2f98881e2e9b
Kevin Lim <lyskevin@gmail.com>
2020-10-06 14:17:12 +0800

    Add solution code and README for Railroad

---
+++ src/Railroad/Main.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        fio.nextInt();
+        int y = fio.nextInt();
+        if (y % 2 == 0) {
+            fio.println("possible");
+        } else {
+            fio.println("impossible");
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Railroad/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/railroad2
+
+## Personal Thoughts
+
+Problem that I attempted during AY20/21 Semester 1. Can be solved by checking if the second number is even.
+
