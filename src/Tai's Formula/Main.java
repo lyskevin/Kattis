d4da7d794447b0084f01da8bafd8e90f593d6499
Kevin Lim <lyskevin@gmail.com>
2020-10-07 23:25:02 +0800

    Add solution code and README for Tai's Formula

---
+++ src/Tai's Formula/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int n = fio.nextInt();
+        double t1 = fio.nextDouble();
+        double v1 = fio.nextDouble();
+        double area = 0.0;
+
+        for (int i = 0; i < n - 1; i++) {
+            double t2 = fio.nextDouble();
+            double v2 = fio.nextDouble();
+            area += 0.5 * (v2 + v1) * (t2 - t1);
+            t1 = t2;
+            v1 = v2;
+        }
+
+        fio.println(area / 1000.0);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
