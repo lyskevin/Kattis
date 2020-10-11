be70633e443d8cd55f08f2710a76d742558bce63
Kevin Lim <lyskevin@gmail.com>
2020-10-11 11:25:56 +0800

    Add solution code and README for Star Arrangements

---
+++ src/Star Arrangements/Main.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        int S = fio.nextInt();
+        fio.println(S + ":");
+        for (int i = 2; i < S; i++) {
+            if (S % ((i << 1) - 1) == 0 || (S - i) % ((i << 1) - 1) == 0) {
+                fio.println(i + "," + (i - 1));
+            }
+            if (S % i == 0) {
+                fio.println(i + "," + i);
+            }
+        }
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Star Arrangements/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/stararrangements
+
+## Personal Thoughts
+
+Problem that I attempted during AY20/21 Semester 1. Can be solved by iterating over possible x, y values and checking if they satisfy the given conditions.
+
