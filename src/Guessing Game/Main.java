cbf7ed2fca29abbf76d055c77eea2e3d9a0bbe30
Kevin Lim <lyskevin@gmail.com>
2020-11-22 23:32:46 +0800

    Add solution code and README for Guessing Game

---
+++ src/Guessing Game/Main.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+        
+        int guess = fio.nextInt();
+        int min = 0;
+        int max = 11;
+        while (guess > 0) {
+            String response = fio.nextLine();
+            if (response.equals("too high")) {
+                max = Math.min(max, guess);
+            } else if (response.equals("too low")) {
+                min = Math.max(min, guess);
+            } else {
+                // Reset here
+                if (min < guess && max > guess) {
+                    fio.println("Stan may be honest");
+                } else {
+                    fio.println("Stan is dishonest");
+                }
+                min = 0;
+                max = 11;
+            }
+            guess = fio.nextInt();
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Guessing Game/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/guessinggame
+
+## Personal Thoughts
+
+Problem that I attempted during AY20/21 Semester 1. Store min and max for each game (i.e. until getting the response "right on") and check that they are consistent with the guess at the end of every game.
+
