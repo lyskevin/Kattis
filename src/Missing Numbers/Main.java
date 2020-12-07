4a7edc444d5c8336b35be62b37fdf2d5d340c78a
Kevin Lim <lyskevin@gmail.com>
2020-12-07 23:50:17 +0800

    Add solution code and README for Missing Numbers

---
+++ src/Missing Numbers/Main.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        boolean[] usedNumbers = new boolean[201];
+        int n = fio.nextInt();
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int number = fio.nextInt();
+            usedNumbers[number] = true;
+            max = Math.max(max, number);
+        }
+
+        boolean hasMissingNumber = false;
+        for (int i = 1; i <= max; i++) {
+            if (!usedNumbers[i]) {
+                hasMissingNumber = true;
+                fio.println(i);
+            }
+        }
+
+        if (!hasMissingNumber) {
+            fio.println("good job");
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Missing Numbers/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/missingnumbers
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Use a boolean array to keep track of numbers which are recited.
+
