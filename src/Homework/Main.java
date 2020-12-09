96810f0f18c70a5a9294e05f144bae0f797d2f96
Kevin Lim <lyskevin@gmail.com>
2020-12-09 16:02:53 +0800

    Add solution code and README for Homework

---
+++ src/Homework/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        String[] ranges = fio.nextLine().split(";");
+        int count = 0;
+        for (String range : ranges) {
+            String[] bounds = range.split("-");
+            if (bounds.length == 1) {
+                count++;
+            } else {
+                count += Integer.parseInt(bounds[1]) - Integer.parseInt(bounds[0]) + 1;
+            }
+        }
+
+        fio.println(count);
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Homework/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/heimavinna
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Split and count the ranges.
+
