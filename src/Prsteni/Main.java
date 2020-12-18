f3e7ab304956f42a017066ac6e79e59a640008b3
Kevin Lim <lyskevin@gmail.com>
2020-12-18 18:48:50 +0800

    Add solution code and README for Prsteni

---
+++ src/Prsteni/Main.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int N = fio.nextInt();
+        int first = fio.nextInt();
+
+        for (int i = 0; i < N - 1; i++) {
+            int next = fio.nextInt();
+            int gcd = gcd(first, next);
+            fio.println((first / gcd) + "/" + (next / gcd));
+        }
+
+        fio.close();
+    }
+
+    private static int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Prsteni/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/prsteni
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Find the GCD of the first ring size with all subsequent ring sizes.
+
