e0b855c05dc306a5e0fd0e3bd85944a04bde9dbf
Kevin Lim <lyskevin@gmail.com>
2020-12-20 10:46:58 +0800

    Add solution code and README for Time Travelling Temperatures

---
+++ src/Time Travelling Temperatures/Main.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int X = fio.nextInt();
+        int Y = fio.nextInt();
+
+        if (X == 0 && Y == 1) {
+            fio.println("ALL GOOD");
+        } else if (Y == 1) {
+            fio.println("IMPOSSIBLE");
+        } else {
+            fio.println(X / (1.0 - Y));
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Time Travelling Temperatures/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/temperature
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Formula is Temperature = X / (1 - Y). Check for the other two cases.
+
