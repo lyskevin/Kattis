9de9e229d17adb19383709c22f9c5f88a8d2af1a
Kevin Lim <lyskevin@gmail.com>
2020-12-20 11:08:51 +0800

    Add solution code and README for Event Planning

---
+++ src/Event Planning/Main.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int N = fio.nextInt();
+        int B = fio.nextInt();
+        int H = fio.nextInt();
+        int W = fio.nextInt();
+
+        int result = Integer.MAX_VALUE;
+
+        for (int i = 0; i < H; i++) {
+            int p = fio.nextInt();
+            for (int j = 0; j < W; j++) {
+                int a = fio.nextInt();
+                int price = N * p;
+                if (a >= N && price <= B) {
+                    result = Math.min(result, price);
+                }
+            }
+        }
+
+        if (result == Integer.MAX_VALUE) {
+            fio.println("stay home");
+        } else {
+            fio.println(result);
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Event Planning/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/eventplanning
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Use two nested loops to get the answer.
+
