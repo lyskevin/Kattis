eb8f6d3c89a51cb109cee96ef6b390fe49eec70d
Kevin Lim <lyskevin@gmail.com>
2020-12-21 15:16:57 +0800

    Add solution code and README for Das Blinkenlights

---
+++ src/Das Blinkenlights/Main.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int p = fio.nextInt();
+        int q = fio.nextInt();
+        int s = fio.nextInt();
+
+        if (lcm(p, q) <= s) {
+            fio.println("yes");
+        } else {
+            fio.println("no");
+        }
+
+        fio.close();
+    }
+
+    private static int gcd(int a, int b) {
+        while (b > 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    private static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Das Blinkenlights/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/dasblinkenlights
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Check if the LCM of p and q is <= s.
+
