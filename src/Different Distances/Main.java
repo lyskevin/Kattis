8223ed695f00d600f640e253e26ebe584ab8cf1d
Kevin Lim <lyskevin@gmail.com>
2020-12-21 15:10:29 +0800

    Add solution code and README for Different Distances

---
+++ src/Different Distances/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        String s = fio.nextLine();
+
+        while (!s.equals("0")) {
+            String[] inputs = s.split(" ");
+            double x1 = Double.parseDouble(inputs[0]);
+            double y1 = Double.parseDouble(inputs[1]);
+            double x2 = Double.parseDouble(inputs[2]);
+            double y2 = Double.parseDouble(inputs[3]);
+            double p = Double.parseDouble(inputs[4]);
+            fio.println(Math.pow(Math.pow(Math.abs(x1 - x2), p) + Math.pow(Math.abs(y1 - y2), p), 1 / p));
+
+            s = fio.nextLine();
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Different Distances/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/differentdistances
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Use power and abs.
+
