0c7d06d8da5964075a21d569237a953a38f2884d
Kevin Lim <lyskevin@gmail.com>
2020-12-21 14:37:36 +0800

    Add solution code and README for Slatkisi

---
+++ src/Slatkisi/Main.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int C = fio.nextInt();
+        int K = fio.nextInt();
+
+        int bill = (int) Math.pow(10, K);
+        int remainder = C % bill;
+        C += remainder == 0 ? 0 : remainder >= bill / 2 ? bill - remainder : -remainder;
+
+        fio.println(C);
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Slatkisi/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/slatkisi
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Rounding with powers of 10.
+
