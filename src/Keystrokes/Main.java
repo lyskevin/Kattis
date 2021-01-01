6a91c2ca7c1059869d98d3668df2393b5a0d55ce
Kevin Lim <lyskevin@gmail.com>
2021-01-01 23:29:30 +0800

    Add solution code and README for Keystrokes

---
+++ src/Keystrokes/Main.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        LinkedList<Character> password = new LinkedList<>();
+        ListIterator<Character> iterator = password.listIterator(0);
+
+        String s = fio.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'L') {
+                iterator.previous();
+            } else if (c == 'R') {
+                iterator.next();
+            } else if (c == 'B') {
+                iterator.previous();
+                iterator.remove();
+            } else {
+                iterator.add(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char c : password) {
+            result.append(c);
+        }
+
+        fio.println(result);
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Keystrokes/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/lyklagangriti
+
+## Personal Thoughts
+
+Problem that I attempted during the winter break of AY20/21. Use LinkedList and ListIterator to construct the password.
+
