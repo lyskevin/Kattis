b4245cd52664f85962d6a299a5bbe7b60b70da1c
Kevin Lim <lyskevin@gmail.com>
2021-01-04 23:43:59 +0800

    Add solution code and README for Luhn's Checksum Algorithm

---
+++ src/Luhn's Checksum Algorithm/Main.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int T = fio.nextInt();
+        for (int i = 0; i < T; i++) {
+            String s = fio.nextLine();
+            int sum = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                int digit = s.charAt(s.length() - 1 - j) - '0';
+                if (j % 2 == 1) {
+                    digit *= 2;
+                    if (digit >= 10) {
+                        digit = digit % 10 + digit / 10;
+                    }
+                }
+                sum += digit;
+            }
+
+            if (sum % 10 == 0) {
+                fio.println("PASS");
+            } else {
+                fio.println("FAIL");
+            }
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
