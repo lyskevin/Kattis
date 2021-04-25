ed7004e161ecda8816abeeea715ddd8ac9d4edf7
Kevin Lim <lyskevin@gmail.com>
2021-04-25 13:38:50 +0800

    add solution code and README for Stopwatch

---
+++ src/Stopwatch/Main.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int n = fio.nextInt();
+        if (n % 2 == 0) {
+            int total = 0;
+
+            for (int i = 0; i < n / 2; i++) {
+                int start = fio.nextInt();
+                int stop = fio.nextInt();
+                total += stop - start;
+            }
+
+            fio.println(total);
+        } else {
+            fio.println("still running");
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Stopwatch/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/stopwatch
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. Follow the instructions in the question. 
+
