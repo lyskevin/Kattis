99b62c0972cc02d8e319126961cf9d9fa165ba1f
Kevin Lim <lyskevin@gmail.com>
2021-05-02 13:00:30 +0800

    add solution code and README for Forced Choice

---
+++ src/Forced Choice/Main.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int N = fio.nextInt();
+        int P = fio.nextInt();
+        int S = fio.nextInt();
+
+        for (int i = 0; i < S; i++) {
+            boolean hasP = false;
+            int m = fio.nextInt();
+            for (int j = 0; j < m; j++) {
+                if (fio.nextInt() == P) {
+                    hasP = true;
+                }
+            }
+            fio.println(hasP ? "KEEP" : "REMOVE");
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Forced Choice/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/forcedchoice
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. At each step, keep the cards if they contain the predicted card, else remove them.
+
