cb7e43c12e8573008136d169c38702566b77ec0b
Kevin Lim <lyskevin@gmail.com>
2021-05-12 15:17:42 +0800

    add solution code and README for Speeding

---
+++ src/Speeding/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int n = fio.nextInt();
+        int max = 0;
+        int t1 = fio.nextInt();
+        int d1 = fio.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            int t2 = fio.nextInt();
+            int d2 = fio.nextInt();
+            max = Math.max(max, (d2 - d1) / (t2 - t1));
+            t1 = t2;
+            d1 = d2;
+        }
+        fio.println(max);
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Speeding/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/speeding
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. Find the maximum among all average speeds.
+
