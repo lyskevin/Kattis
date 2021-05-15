bee3a94bdf935c5afcb7a066bf0d968ff84a21db
Kevin Lim <lyskevin@gmail.com>
2021-05-15 23:54:30 +0800

    Add solution code and README for Paul Eigon

---
+++ src/Paul Eigon/Main.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int N = fio.nextInt();
+        int P = fio.nextInt();
+        int Q = fio.nextInt();
+
+        fio.println(((P + Q) / N) % 2 == 0 ? "paul" : "opponent");
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Paul Eigon/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/pauleigon
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. It is Paul's tuen to serve if floor((P + Q) / N) is even.
+
