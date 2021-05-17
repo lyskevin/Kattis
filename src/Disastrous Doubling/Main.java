4544a5f5167d10eead18a3757286d059552371d2
Kevin Lim <lyskevin@gmail.com>
2021-05-17 10:33:15 +0800

    Add solution code and README for Disastrous Doubling

---
+++ src/Disastrous Doubling/Main.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int n = fio.nextInt();
+        BigInteger result = new BigInteger("1");
+        boolean hasError = false;
+
+        for (int i = 0; i < n && !hasError; i++) {
+            result = result.multiply(BigInteger.TWO);
+            BigInteger required = new BigInteger(fio.next());
+            if (result.compareTo(required) < 0) {
+                hasError = true;
+            } else {
+                result = result.subtract(required);
+            }
+        }
+
+        if (hasError) {
+            fio.println("error");
+        } else {
+            fio.println(result.mod(new BigInteger("1000000007")));
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
