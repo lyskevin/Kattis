3bc7db12b896f3d5bb0ac6bfa4a4d2e29c270db1
Kevin Lim <lyskevin@gmail.com>
2021-05-30 22:20:15 +0800

    Add solution code and README for Magic Trick

---
+++ src/Magic Trick/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        boolean[] chars = new boolean[256];
+        String s = fio.nextLine();
+        boolean hasRepeat = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (chars[s.charAt(i)]) {
+                hasRepeat = true;
+            } else {
+                chars[s.charAt(i)] = true;
+            }
+        }
+
+        fio.println(hasRepeat ? 0 : 1);
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Magic Trick/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/magictrick
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. Print 0 if all letters in the string are unique, else 1.
+
