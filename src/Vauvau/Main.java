8f21560d06781ae7e3e8ca4aec24ada326bad0ce
Kevin Lim <lyskevin@gmail.com>
2021-05-31 23:36:40 +0800

    Add solution code and README for Vauvau

---
+++ src/Vauvau/Main.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int A = fio.nextInt();
+        int B = fio.nextInt();
+        int C = fio.nextInt();
+        int D = fio.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            int time = fio.nextInt();
+            int count = 0;
+
+            if ((time - 1) % (A + B) < A) {
+                count++;
+            }
+
+            if ((time - 1) % (C + D) < C) {
+                count++;
+            }
+
+            if (count == 0) {
+                fio.println("none");
+            } else if (count == 1) {
+                fio.println("one");
+            } else {
+                fio.println("both");
+            }
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Vauvau/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/vauvau
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. The timing windows can be calculated using modular arithmetic.
+
