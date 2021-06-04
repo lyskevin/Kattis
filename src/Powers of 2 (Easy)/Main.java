6bb85e7a5dffb5f6e1c6943830c9e67a99f4021a
Kevin Lim <lyskevin@gmail.com>
2021-06-04 22:29:11 +0800

    Add solution code and README for Powers of 2

---
+++ src/Powers of 2 (Easy)/Main.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int k = fio.nextInt();
+        int e = fio.nextInt();
+        String power = ((int) Math.pow(2, e)) + "";
+        int count = 0;
+
+        for (int i = 0; i <= k; i++) {
+            if ((i + "").indexOf(power) > -1) {
+                count++;
+            }
+        }
+
+        fio.println(count);
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Powers of 2 (Easy)/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/powersof2easy
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. Brute force the checks for numbers in the range [0, k].
+
