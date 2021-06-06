0f1870bd8655fb69d0bac9c15d537856a5aa3a9b
Kevin Lim <lyskevin@gmail.com>
2021-06-07 00:06:15 +0800

    Add solution code and README for Hay Points

---
+++ src/Hay Points/Main.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int m = fio.nextInt();
+        int n = fio.nextInt();
+        HashMap<String, Integer> dictionary = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            dictionary.put(fio.next(), fio.nextInt());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            String input = fio.nextLine();
+
+            while (!input.equals(".")) {
+                String[] words = input.split(" ");
+
+                for (String word : words) {
+                    sum += dictionary.containsKey(word) ? dictionary.get(word) : 0;
+                }
+
+                input = fio.nextLine();
+            }
+
+            fio.println(sum);
+        }
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Hay Points/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/haypoints
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. Use a HashMap<String, Integer> to store each dictionary word and its dollar value.
+
