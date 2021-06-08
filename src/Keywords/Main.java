276b9921f6f8f527e7cfe675f7757591c9a1135d
Kevin Lim <lyskevin@gmail.com>
2021-06-08 23:42:22 +0800

    Add solution code and README for Keywords

---
+++ src/Keywords/Main.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author lyskevin
+ */
+public class Main {
+    public static void main(String[] args) {
+        FastIO fio = new FastIO();
+
+        int n = fio.nextInt();
+        HashSet<String> words = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String s = fio.nextLine().toUpperCase();
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < s.length(); j++) {
+                sb.append(s.charAt(j) == '-' ? ' ' : s.charAt(j));
+            }
+
+            words.add(sb.toString());
+        }
+
+        fio.println(words.size());
+
+        fio.close();
+    }
+}
+
+/**
+ * Fast I/O
+ * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+ */
+class FastIO extends PrintWriter 
+{ 
+    BufferedReader br; 
+    StringTokenizer st;
+
+    public FastIO() 
+    { 
+        super(new BufferedOutputStream(System.out)); 
+        br = new BufferedReader(new
+                InputStreamReader(System.in));
+    } 
+
+    String next() 
+    { 
+        while (st == null || !st.hasMoreElements()) 
+        { 
+            try
+            { 
+                st = new StringTokenizer(br.readLine()); 
+            } 
+            catch (IOException  e) 
+            { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    int nextInt() 
+    { 
+        return Integer.parseInt(next()); 
+    } 
+
+    long nextLong() 
+    { 
+        return Long.parseLong(next()); 
+    } 
+
+    double nextDouble() 
+    { 
+        return Double.parseDouble(next()); 
+    } 
+
+    String nextLine() 
+    { 
+        String str = ""; 
+        try
+        { 
+            str = br.readLine(); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        return str; 
+    } 
+}
+
---
+++ src/Keywords/README.md
@@ -0,0 +1,8 @@
+# Link
+
+https://open.kattis.com/problems/keywords
+
+## Personal Thoughts
+
+Problem that I attempted during the summer break of AY20/21. Process the words and use a HashSet to keep track of unique words.
+
